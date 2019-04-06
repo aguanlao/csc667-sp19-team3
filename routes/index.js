6e64afa7575c31c0db559de0aa92496cc4a900b7
motiveg <bcasipit91@gmail.com>
2019-04-05 19:36:03 -0700

    added routes for pug views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Index' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register' });
+});
+
+router.get('/lobby', function(req, res, next) {
+  res.render('lobby', { title: 'Lobby' });
+});
+
+router.get('/game', function(req, res, next) {
+  res.render('game', { title: 'Game' });
 });
 
 module.exports = router;
