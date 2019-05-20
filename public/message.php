4a993b2e269e10e7691f70d28d4ebe07863ee5dd
Jay Lee <jlee60@mail.sfsu.edu>
2019-05-20 02:06:41 -0700

    changed the order of reading and writing message
    if you want to make the chat perfect, crate a copy of this message.php and rename it something like message_read.php. Cut line 21-32 from message.php and paste in message_read.php. and edit message.html to load message_read.php only when message should be read and not written to database.

--- public/message.php
+++ public/message.php
@@ -6,20 +6,16 @@ $DB['pw'] = 'sfstatecsc667team03';
 
 $mysqli = new mysqli($DB['host'], $DB['id'], $DB['pw'], $DB['db']);
 
-$message = trim($_POST['message']);
+$message = '';
+
+if (!empty($_POST['message'])) {
+  $message = trim($_POST['message']);
+}
 //TODO
 //$uid = $_POST['uid'];
 //$gid = $_POST['gid'];
 
 
-if(isset($message) === true && $message === '') {
-  $mysqli->close();
-  exit();
-}
-
-//mid (auto_increment) | uid | gid | message | timestamp(defalt=CURRENT_TIMESTAMP)
-$q = "INSERT INTO message (uid, gid, message) VALUES ('9999', '9999', '$message')";
-$mysqli->query($q);
 
 //retrieve messages associated with the requested gid
 $q = "SELECT * FROM message WHERE gid LIKE '%9999%'";
@@ -28,6 +24,17 @@ $result = $mysqli->query($q);
 while($row = mysqli_fetch_array($result)) {
   echo "[".$row['timestamp']."] ".$row['uid'].": ".$row['message']."\r\n";
 }
+
+//validate new message string
+if(isset($message) === true && $message === '') {
+  $mysqli->close();
+  exit();
+}
+
+//mid (auto_increment) | uid | gid | message | timestamp(defalt=CURRENT_TIMESTAMP)
+$q = "INSERT INTO message (uid, gid, message) VALUES ('User', '9999', '$message')";
+$mysqli->query($q);
+
 
 $mysqli->close();
 exit();
