b6a18aaf445d48e3aec3e5c6a68d402f4d97cee4
Jay Lee <jlee60@mail.sfsu.edu>
2019-05-20 01:20:18 -0700

    Reformatted indentation in api.js
    Indent by 2 spaces at a time. - Google JavaScript Style Guide (https://google.github.io/styleguide/jsguide.html)

--- routes/api.js
+++ routes/api.js
@@ -5,20 +5,21 @@ var router = express.Router();
 // TEMPORARY: probably not the best practice to place this directly in routes?
 var mysql = require('mysql');
 function getConnection() {
-    return mysql.createConnection({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME
-    });
+  return mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  });
 }
 
-app.use('/api', router); // <oururl>.com/api is the base url for all our api routes
+// <oururl>.com/api is the base url for all our api routes
+app.use('/api', router);
 
 router.get('/lobby', function (req, res, next) {
-    // TODO
-    // - get json of: lobby number, player info, lobby status
+  // TODO
+  // - get json of: lobby number, player info, lobby status
 });
 
 module.exports = router;
